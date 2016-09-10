cb96046a57ee38649cf465f7c7ac665f924d64f9
David Prior <david.prior@davecheesefish.com>
2016-09-10 18:17:38 +0100

    Updated main.js to register Numerousness.js as an AMD module (instead of registering a global) if AMD is being used.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
 require(['core', 'numbers'],function(Numerousness){
 	
-	// Create global object
-	window.Numerousness = Numerousness;
+	// If AMD is available, define a module, otherwise create a global object.
+	// AMDclean breaks this section, so the following comment forces it to ignore the next line:
+	// amdclean
+	if (typeof define == 'function' && define.amd){
+		// amdclean
+		define(function(){
+			return Numerousness;
+		});
+	} else if (typeof exports !== 'undefined' && module.exports){
+		exports = module.exports = Numerousness;
+	} else {
+		window.Numerousness = Numerousness;
+	}
+	
 });
